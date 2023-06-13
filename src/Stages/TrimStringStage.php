e48f7efdbcd13e48027df1d4d581630b2d91cf8b
Kari Laari <kari@karin-atk.fi>
2023-06-13 10:58:46 +0300

    Fix empty space trimming
    It was breaking the encoding with some special characters

--- src/Stages/TrimStringStage.php
+++ src/Stages/TrimStringStage.php
@@ -12,25 +12,10 @@ class TrimStringStage implements StageInterface
 {
 
     /**
-     * @var string
-     */
-    private $characterMask;
-
-    /**
-     * TrimStringStage constructor.
-     *
-     * @param string $characterMask
-     */
-    public function __construct(string $characterMask = " \t\n\r\0\x0B\xC2\xA0")
-    {
-        $this->characterMask = $characterMask;
-    }
-
-    /**
      * @inheritDoc
      */
     public function __invoke($payload)
     {
-        return \trim($payload, $this->characterMask);
+        return \trim(preg_replace('/^\s+|\s+$/u', ' ', "$payload\u{a0}"));
     }
 }
